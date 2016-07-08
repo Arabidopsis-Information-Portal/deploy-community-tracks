020f1ac0ce05a09952d1a8d021aa9bf082ccd924
eriksf <eferlanti@tacc.utexas.edu>
2016-07-08 14:09:38 -0500

    added gff3 validation using genometools

--- src/process_gff.sh
+++ src/process_gff.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+set -e
+
 FULL_GFF=$1
-BASE_GFF=$(basename $FULL_GFF)
+BASE_GFF=$(basename "$FULL_GFF")
 NAME_GFF=${BASE_GFF%.*}
 SORTED_GFF=$NAME_GFF.sorted.gff
 
-# sort the gff
-gt gff3 -sortlines yes -tidy yes -retainids yes -addids no -checkids yes $FULL_GFF >$SORTED_GFF
+# validate the gff file (using genometools)
+gt gff3validator -typecheck so "$FULL_GFF"
+
+# sort the gff (using genometools)
+#gt gff3 -sortlines yes -tidy yes -retainids yes -addids no -checkids yes $FULL_GFF >$SORTED_GFF
+
+# sort the gff (using sort)
+(grep ^"#" "$FULL_GFF"; grep -v ^"#" "$FULL_GFF" | sort -k1,1 -k4,4n) >"$SORTED_GFF"
 
 # compress and index
-bgzip $SORTED_GFF
-tabix -p gff $SORTED_GFF.gz
+bgzip "$SORTED_GFF"
+tabix -p gff "$SORTED_GFF.gz"
